537a93a27e0df5422df0bc72026ec92c7cc64a90
Ian Terrell <ian.terrell@willowtreeapps.com>
2016-07-28 13:32:44 -0400

    Add slides

--- Example/Pods/Target Support Files/Cordux/Cordux-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Cordux : NSObject
-@end
-@implementation PodsDummy_Cordux
-@end
--- Example/Pods/Target Support Files/Pods-CorduxPrototype/Pods-CorduxPrototype-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CorduxPrototype : NSObject
-@end
-@implementation PodsDummy_Pods_CorduxPrototype
-@end
