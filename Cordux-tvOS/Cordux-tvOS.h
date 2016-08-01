03d0ebbafa97eb620656c1ef8dc609e376c40477
Ian Terrell <ian.terrell@willowtreeapps.com>
2016-08-01 11:21:56 -0400

    Add tvOS support

---
+++ Cordux-tvOS/Cordux-tvOS.h
@@ -0,0 +1,19 @@
+//
+//  Cordux-tvOS.h
+//  Cordux-tvOS
+//
+//  Created by Ian Terrell on 8/1/16.
+//  Copyright © 2016 WillowTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Cordux-tvOS.
+FOUNDATION_EXPORT double Cordux_tvOSVersionNumber;
+
+//! Project version string for Cordux-tvOS.
+FOUNDATION_EXPORT const unsigned char Cordux_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Cordux_tvOS/PublicHeader.h>
+
+
